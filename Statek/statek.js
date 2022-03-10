dae7ff72f56857f8fa18b6e7daf5717e8f5173a2
AgataFrancik <agata.francik@interia.pl>
2022-03-10 11:04:20 +0100

    Ship mocement added

--- Statek/statek.js
+++ Statek/statek.js
@@ -20,7 +20,26 @@ Game = {
         window.addEventListener('resize', Game.layout);
         document.body.appendChild(Game.canvas);
         Game.ship = new Ship();
+
+        window.addEventListener('keyup', Game.onKey, false);
+        window.addEventListener('keydown', Game.onKey, false);
+
         Game.animationLoop();
+    },
+    onKey: function(event){
+        if(event.keyCode ==32 || event.keyCode == 37|| event.keyCode == 38 || event.keyCode == 39){
+            event.preventDefault();
+            if(event.type=='keydown' && !Game['key_'+event.keyCode]){
+                Game['key_'+event.keyCode] = true;
+                if(event.keyCode == 37){
+                    Game.key_39 =false;
+                } else if(event.keyCode == 39){
+                    Game.key_37 =false;
+                }
+            } else if(event.type == 'keyup'){
+                Game['key_'+event.keyCode]=false;
+            }
+        }
     },
     layout: function(ev){
         VAR.W = window.innerWidth;
@@ -37,7 +56,7 @@ Game = {
         requestAnimationFrame(Game.animationLoop);
         if(time - VAR.lastTime >= 1000/VAR.fps){
             VAR.lastTime = time;
-            Game.ctx.clearRect(0,0,VAR.w, VAR.H);
+            Game.ctx.clearRect(0,0,VAR.W, VAR.H);
             Game.ship.draw();
         }
     }
