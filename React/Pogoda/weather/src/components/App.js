30e6e7d78bbaa0085f9a2e6abb024c0c069ada91
AgataFrancik <agata.francik@interia.pl>
2022-03-23 22:20:59 +0100

    Add state update

--- React/Pogoda/weather/src/components/App.js
+++ React/Pogoda/weather/src/components/App.js
@@ -2,21 +2,68 @@ import React, {Component} from 'react';
 import Form from './Form';
 import Result from './Result'
 import './App.css';
+const APIkey = 'db7441a621b9f44e31e9a7475305a9c7'
 
 class App extends Component {
   state = {
-    value: ""
+    value: '',
+    date: '',
+    city: '',
+    sunrise: '',
+    sunset: '',
+    temp: '',
+    wind: '',
+    pressure: '',
+    err: false
   }
   chandleInputChange = (e) =>{
     this.setState({
       value: e.target.value
     })
   }
+  chandleCitySubmit = e =>{
+    e.preventDefault()
+    console.log("potwierdzono");
+    const API = 'https://api.openweathermap.org/data/2.5/weather?q= '+this.state.value+' &appid='+APIkey+'&units=metric';
+
+    fetch(API)
+    .then(response => {
+      if(response.ok){
+        return response
+      }
+      throw Error("Nie udało się");
+    })
+    .then(response => response.json())
+    .then(data =>{
+      const date = new Date().toLocaleString()
+      this.setState({
+        err: false,
+        date: date,
+        city: this.state.value,
+        sunrise: data.sys.sunrise,
+        sunset: data.sys.sunset,
+        temp: data.main.temp,
+        wind: data.wind.speed,
+        pressure: data.main.pressure
+      })
+    })
+    .catch(err => {
+      this.setState(state => ({ 
+        err: true,
+        city: this.state.value
+      }))
+    })
+  }
   render(){
   return (
     <div className="App">
-     <Form value={this.state.value} change={this.chandleInputChange}/>
-     <Result/>
+     <Form value={this.state.value} 
+     change={this.chandleInputChange}
+     submit={this.chandleCitySubmit}
+     />
+     <Result
+      weather={this.state}
+     />
     </div>
   );
   }
--- React/Pogoda/weather/src/components/Result.js
+++ React/Pogoda/weather/src/components/Result.js
@@ -1,11 +1,16 @@
 import React from "react";
 
-const Result = () => {
+const Result = (props) => {
+    const {err, city, temp, sunrise, sunset, wind, pressure, date} = props.weather;
     return( 
+        <React.Fragment>
        <div>
-           Rezultat
+           Pogoda dla: {city}
        </div>
-
+       <div>
+           Temperatura: {temp}
+       </div>
+       </React.Fragment>
     );
 }
 export default Result;
